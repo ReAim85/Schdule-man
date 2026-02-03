24ecffb3b8281eba55654f58113b8cd1ab2cb4ca
reaim85 <visual19852004@gmail.com>
2026-02-03 14:45:14 +0530

    tried to use the native document picker but it didn't work. going to start using expo document picker

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,41 @@
+import * as DocumnetPicker from "expo-document-picker";
+import { useState } from "react";
 import { Button, Text, View } from "react-native";
+import PDF from "react-native-pdf";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button title="Select 📑"></Button>
-    </View>
-  );
+  const [fileUri, setFileUri] = useState("");
+  const handlePick = async () => {
+    const res = await DocumnetPicker.getDocumentAsync({});
+    //@ts-ignore
+    await setFileUri(res.uri);
+    console.log(fileUri);
+  };
+  if (fileUri == "") {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Button onPress={handlePick} title="Select 📑"></Button>
+      </View>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* @ts-ignore */}
+        <PDF source={fileUri} style={{ flex: 1 }}></PDF>
+      </View>
+    );
+  }
 }
