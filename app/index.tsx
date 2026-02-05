970075614618e48f4627e6a3d3c0f28ebff571a5
reaim85 <visual19852004@gmail.com>
2026-02-05 23:59:22 +0530

    The basic of this project has been completed. New features to add: 1. An OCR which extracts days and time of all the classes and based on current timing send notificaiton. 2. An App widget which will update every hour to show next classes and current class

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,71 @@
-import * as DocumnetPicker from "expo-document-picker";
-import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as DocumentPicker from "expo-document-picker";
+import { useEffect, useState } from "react";
 import { Button, Dimensions, StyleSheet, Text, View } from "react-native";
 import PDF from "react-native-pdf";
 
+const STORE_KEY = "@timetable_URI";
+
 export default function Index() {
   const [fileUri, setFileUri] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    loadSavedUri();
+  });
+
+  const loadSavedUri = async () => {
+    try {
+      const savedUri = await AsyncStorage.getItem(STORE_KEY);
+      if (savedUri !== null) {
+        setFileUri(savedUri);
+      }
+    } catch (err) {
+      console.error("error loading uri: ", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const saveUri = async (uri: string) => {
+    try {
+      await AsyncStorage.setItem(STORE_KEY, uri);
+    } catch (err) {
+      console.error("error saving URI: ", err);
+    }
+  };
+
   const handlePick = async () => {
-    const res = await DocumnetPicker.getDocumentAsync({});
-    if (!res.canceled) {
-      //@ts-ignore
-      await setFileUri(res.uri);
+    const res = await DocumentPicker.getDocumentAsync({
+      type: "application/pdf",
+    });
+    if (!res.canceled && res.assets && res.assets.length > 0) {
+      setFileUri(res.assets[0].uri);
+      saveUri(res.assets[0].uri);
       console.log(fileUri);
     } else {
       console.log("document selection cancled!");
     }
   };
-  if (fileUri == "") {
+
+  const handleReset = async () => {
+    try {
+      await AsyncStorage.removeItem(STORE_KEY);
+      setFileUri(null);
+    } catch (err) {
+      console.error("error reseting URI", err);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={style.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (fileUri === null) {
     return (
       <View style={style.container}>
         <Text>No timetable selected yet!</Text>
@@ -39,7 +89,7 @@ export default function Index() {
       />
       {/*button to reset (i might need it)*/}
       <View style={{ position: "absolute", bottom: 20 }}>
-        <Button title="Change File" onPress={() => setFileUri(null)} />
+        <Button title="Change File" onPress={handleReset} />
       </View>
     </View>
   );
